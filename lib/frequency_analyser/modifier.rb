9e86a752d4220552d6fd15d36e9df59f828ed230
Chris Patuzzo <chris@patuzzo.co.uk>
2012-09-16 11:16:00 +0100

    Allow modifier to carry state for brevity

--- lib/frequency_analyser/modifier.rb
+++ lib/frequency_analyser/modifier.rb
@@ -1,37 +1,35 @@
-class FrequencyAnalyser::Modifier
+class FrequencyAnalyser::Modifier < Struct.new(:aggregation)
 
   def self.modify(aggregation, mode)
-    new.modify(aggregation, mode)
+    new(aggregation).modify(mode)
   end
 
-  def modify(aggregation, mode)
+  def modify(mode)
     case mode
     when :probability
-      probability(aggregation)
+      probability
     when :percentage
-      percentage(aggregation)
+      percentage
     else
       aggregation
     end
   end
 
   private
-  def probability(aggregation)
-    sum = sum(aggregation)
+  def probability
     aggregation.inject(aggregation) do |hash, (k, v)|
       hash.merge!(k => v.to_f / sum)
     end
   end
 
-  def percentage(aggregation)
-    sum = sum(aggregation)
+  def percentage
     aggregation.inject(aggregation) do |hash, (k, v)|
       hash.merge!(k => v.to_f / sum * 100)
     end
   end
 
-  def sum(aggregation)
-    aggregation.values.inject(:+)
+  def sum
+    @sum ||= aggregation.values.inject(:+)
   end
 
 end
