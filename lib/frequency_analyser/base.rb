e9d1da2cb90b284d0a91b514d98237a17e44dcd3
Chris Patuzzo <chris@patuzzo.co.uk>
2012-09-16 09:50:54 +0100

    Add support for probability and percentage modes

--- lib/frequency_analyser/base.rb
+++ lib/frequency_analyser/base.rb
@@ -4,20 +4,57 @@ class FrequencyAnalyser < Struct.new(:counter, :aggregation)
     super
   end
 
-  def self.analyse(*files)
-    new.analyse(*files)
+  def self.analyse(*args)
+    new.analyse(*args)
   end
 
-  def analyse(*files)
-    files = *files.flatten
+  def analyse(*args)
+    files, mode = coerce(args)
+    aggregation = aggregate(files)
 
+    case mode
+    when :probability
+      probability(aggregation)
+    when :percentage
+      percentage(aggregation)
+    else
+      aggregation
+    end
+  end
+
+  private
+  def coerce(args)
+    files = [args].flatten
+    mode = files.pop if files.last.is_a? Symbol
+
+    [files, mode]
+  end
+
+  def aggregate(files)
     files.each do |file|
       file.each_line do |line|
         aggregation << counter.count(line)
       end
     end
-
     aggregation
+  end
+
+  def probability(aggregation)
+    sum = sum(aggregation)
+    aggregation.inject(aggregation) do |hash, (k, v)|
+      hash.merge!(k => v.to_f / sum)
+    end
+  end
+
+  def percentage(aggregation)
+    sum = sum(aggregation)
+    aggregation.inject(aggregation) do |hash, (k, v)|
+      hash.merge!(k => v.to_f / sum * 100)
+    end
+  end
+
+  def sum(aggregation)
+    aggregation.values.inject(:+)
   end
 
 end
--- spec/frequency_analyser/base_spec.rb
+++ spec/frequency_analyser/base_spec.rb
@@ -12,13 +12,39 @@ describe FrequencyAnalyser do
     analyser.aggregation.should == aggregation
   end
 
-  it 'returns a summation of character frequencies in the file' do
-    file = StringIO.new("foo BAR\nbaz")
-    analysis = subject.analyse(file)
+  describe 'modes' do
 
-    analysis['a'].should == 2
-    analysis['b'].should == 2
-    analysis['f'].should == 1
+    it 'returns a summation of character frequency by default' do
+      file = StringIO.new("foo BAR\nbaz")
+      analysis = subject.analyse(file)
+
+      analysis['a'].should == 2
+      analysis['b'].should == 2
+      analysis['f'].should == 1
+    end
+
+    it 'has a probability mode' do
+      file = StringIO.new("aaaabbbccd")
+      analysis = subject.analyse(file, :probability)
+
+      analysis['a'].should == 0.4
+      analysis['b'].should == 0.3
+      analysis['c'].should == 0.2
+      analysis['d'].should == 0.1
+      analysis['e'].should == 0
+    end
+
+    it 'has a percentage mode' do
+      file = StringIO.new("aaaabbbccd")
+      analysis = subject.analyse(file, :percentage)
+
+      analysis['a'].should == 40
+      analysis['b'].should == 30
+      analysis['c'].should == 20
+      analysis['d'].should == 10
+      analysis['e'].should == 0
+    end
+
   end
 
 end
